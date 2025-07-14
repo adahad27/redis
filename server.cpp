1f7b751fa9592bbb5c9aa8728876a8883775cc67
Abhishek <abhishek.dahad@gmail.com>
2025-07-14 17:23:33 -0400

    Added read_all, send_all functions to handle partial message receiving/sending.

--- server.cpp
+++ server.cpp
@@ -4,8 +4,59 @@
 #include <netinet/tcp.h>
 #include <cassert>
 #include <unistd.h>
+#include <string.h>
 
 const uint32_t MAX_MSG_LEN = 1000; 
+
+int recv_all(int connfd, char* buf, int n) {
+    int read_bytes = 0;
+    int result = 0;
+    while(read_bytes < n) {
+        result = recv(connfd, buf, n, 0);
+        if(result < 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int send_all(int connfd, char* buf, int n) {
+    int write_bytes = 0;
+    int result = 0;
+    while(write_bytes < n) {
+        result = send(connfd, buf, n, 0);
+        if(result < 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int req_resp_handler(int connection_fd) {
+    char msg[4 + MAX_MSG_LEN];
+
+    int error = recv_all(connection_fd, msg, 4);
+
+    if(error < 0) {
+        return error;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, msg, 4);
+
+    if(len >= MAX_MSG_LEN) {
+        return -1;
+    }
+
+    error = recv_all(connection_fd, msg + 4, len);
+    
+    if(error < 0) {
+        return error;
+    }
+
+
+
+}
 
 int main() {
 
