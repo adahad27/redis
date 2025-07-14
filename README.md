5ce5d82672243dabb802ac02fe4eccf7a4d2f08a
Abhishek <abhishek.dahad@gmail.com>
2025-07-13 20:31:01 -0400

    Added README.md Created service loop within server.cpp.

---
+++ README.md
@@ -0,0 +1,2 @@
+This project was started on July 17th, 2025.
+The goal of this project is to create a Redis server from scratch. More information will be added as the scope of the project becomes clear.
--- server.cpp
+++ server.cpp
@@ -2,6 +2,10 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <cassert>
+#include <unistd.h>
+
+const uint32_t MAX_MSG_LEN = 1000; 
 
 int main() {
 
@@ -10,4 +14,28 @@ int main() {
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
+    uint32_t port = 80;
+
+    sockaddr_in addr{};
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+
+    int network_status = bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    //Add an assert that checks network status here.
+    assert(network_status);
+
+    listen(fd, MAX_MSG_LEN);
+
+    struct sockaddr_in client{};
+    socklen_t addrlen = sizeof(client);
+
+    while(true) {
+        int connection_fd = accept(fd, reinterpret_cast<sockaddr *>(&client), &addrlen);
+        assert(connection_fd);
+        //Redis work
+        close(connection_fd);
+    }
 }
