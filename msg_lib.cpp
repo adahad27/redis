78316359edcd9e790eb67fef5d91cf26580ca90c
Abhishek <abhishek.dahad@gmail.com>
2025-07-26 21:02:48 -0400

    Added RPCs to send commands to server from client.

--- msg_lib.cpp
+++ msg_lib.cpp
@@ -27,3 +27,87 @@ int send_all(int connfd, char* buf, int n) {
     return 0;
 }
 
+int establish_connection() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(fd);
+    
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(port);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+
+    int err_status = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    assert(err_status >= 0);
+
+    return fd;
+}
+
+void get_request(std::string key, char* response, int &response_len) {
+    
+    int fd = establish_connection();
+
+    char command[] = "get";
+    char *arg = key.data();
+
+    int command_len = strlen(command);
+    int arg_len = strlen(arg);
+
+    int num_cmd = 2;
+    send_all(fd, (char*)&num_cmd, 4);
+
+
+    send_all(fd, (char *) &command_len, 4);
+    send_all(fd, command, command_len);
+
+    send_all(fd, (char*) &arg_len, 4);
+    send_all(fd, arg, arg_len);
+
+    recv_all(fd, (char*) &response_len, 4);
+    recv_all(fd, response, response_len);
+
+
+}
+
+void del_request(std::string key) {
+    int fd = establish_connection();
+
+    char command[] = "del";
+    char *arg = key.data();
+
+    int command_len = strlen(command);
+    int arg_len = strlen(arg);
+
+    int num_cmd = 2;
+    send_all(fd, (char*)&num_cmd, 4);
+
+    send_all(fd, (char *) &command_len, 4);
+    send_all(fd, command, command_len);
+
+    send_all(fd, (char*) &arg_len, 4);
+    send_all(fd, arg, arg_len);
+}
+
+void set_request(std::string key, std::string value) {
+    int fd = establish_connection();
+
+    char command[] = "set";
+    char *arg_1 = key.data();
+    char *arg_2 = value.data();
+
+    int command_len = strlen(command);
+    int arg_1_len = strlen(arg_1);
+    int arg_2_len = strlen(arg_2);
+
+    int num_cmd = 3;
+    send_all(fd, (char*)&num_cmd, 4);
+
+    send_all(fd, (char *) &command_len, 4);
+    send_all(fd, command, command_len);
+
+    send_all(fd, (char*) &arg_1_len, 4);
+    send_all(fd, arg_1, arg_1_len);
+
+    send_all(fd, (char*) &arg_2_len, 4);
+    send_all(fd, arg_2, arg_2_len);
+}
--- msg_lib.hpp
+++ msg_lib.hpp
@@ -13,16 +13,19 @@
 #include <vector>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <string>
 
 const uint32_t port = 8080;
 const uint32_t MAX_MSG_LEN = 1000;
 
-int recv_all(int connfd, char* buf, int n);
+// int recv_all(int connfd, char* buf, int n);
 
-int send_all(int connfd, char* buf, int n);
+// int send_all(int connfd, char* buf, int n);
 
+void get_request(std::string key, char* response, int &response_len);
 
+void del_request(std::string key);
 
-
+void set_request(std::string key, std::string value);
 
 #endif
